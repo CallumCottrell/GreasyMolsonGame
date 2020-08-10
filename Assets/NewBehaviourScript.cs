97906caca220b7f1ce1fac66f2b10b3463d36cb3
Callum Cottrell <callum.cottrell@mac.com>
2020-08-10 14:25:17 -0400

    first commit

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Joey is a dummy
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
