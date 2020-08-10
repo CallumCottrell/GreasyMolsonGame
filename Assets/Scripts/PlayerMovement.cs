3e1988ceaf626ce2752268dfe3b6988415fa661e
Philip Teri <41071502+PhilipTeri@users.noreply.github.com>
2020-08-10 15:02:39 -0400

    dfdf

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+
+    
+    public CharacterController controller;
+
+    public float speed = 12f;
+    public float gravity = -9.81f;
+
+    
+
+
+
+    public Transform groundCheck;
+
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+    //public float jumpHeight = 3f;
+
+    
+    Vector3 velocity;
+
+    bool isGrounded;
+
+    public float jumpVelocity;
+
+    public float fallMultiplier = 2.5f;
+    public float lowJumpMultiplier = 2f;
+ 
+    // Update is called once per frame
+    void Update()
+    {
+
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y <0)
+        {
+            velocity.y = -2f;
+        } 
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * speed * Time.deltaTime);
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity = Vector3.up * jumpVelocity;
+        };
+ 
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
+
+        if(velocity.y < 0)
+        {
+        velocity += Vector3.up * Physics.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+        }
+
+        else if (velocity.y > 0 && !Input.GetButton ("Jump")) {
+            velocity += Vector3.up * Physics.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
+
+
+        }
+        
+    }
+}
