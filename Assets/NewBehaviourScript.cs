f0c85bdf5cc6593a7b8a0fbde9de54cd95f31641
Philip Teri <41071502+PhilipTeri@users.noreply.github.com>
2020-08-10 14:49:50 -0400

    Update NewBehaviourScript.cs

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -7,7 +7,7 @@ public class NewBehaviourScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //Joey is a dummy
+        //Joey is a dummy (true)
 
         
     }
