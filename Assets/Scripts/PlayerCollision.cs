cd08ad8a69b4fd56669dc56d3165e23c05cde49f
Callum Cottrell <callum.cottrell@mac.com>
2020-08-10 17:58:38 -0400

    better hit collision with the tree

--- Assets/Scripts/PlayerCollision.cs
+++ Assets/Scripts/PlayerCollision.cs
@@ -3,8 +3,7 @@ using UnityEngine;
 
 public class PlayerCollision : MonoBehaviour
 {
-    void OnCollisionEnter(){
-        Debug.Log("test");
-        UnityEngine.Debug.Log("test");
+    void OnCollisionEnter(Collision hit){
+        UnityEngine.Debug.Log(hit.gameObject.name);
     }
 }
