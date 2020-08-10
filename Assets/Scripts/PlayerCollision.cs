f06bd293c7d1a8833782348c0095b1fd6f83d795
Philip Teri <41071502+PhilipTeri@users.noreply.github.com>
2020-08-10 19:49:01 -0400

    press E added

--- Assets/Scripts/PlayerCollision.cs
+++ Assets/Scripts/PlayerCollision.cs
@@ -3,10 +3,29 @@ using UnityEngine;
 
 public class PlayerCollision : MonoBehaviour
 {
+    
+    public GameObject FloatingTextPrefab;
+    public Transform playerBody;
+
     void OnCollisionEnter(Collision hit){
         UnityEngine.Debug.Log(hit.gameObject.name);
-        if (hit.gameObject.name == "tree"){
-            
+        if (hit.gameObject.name == "tree"){  
+        }
+        
+        if (FloatingTextPrefab)
+        {
+        showFloatingText();
+        
         }
     }
+  
+
+    
+    
+    void showFloatingText()
+    {
+        Instantiate(FloatingTextPrefab, transform.position, Quaternion.identity, transform);
+    }
+
 }
+
