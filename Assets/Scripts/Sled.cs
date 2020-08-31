391e2c32d24d7c33c0bf4d3f53ad99121bebdd2e
Callum Cottrell <callum.cottrell@mac.com>
2020-08-31 18:01:26 -0400

    sled can slide downhill sort of

--- Assets/Scripts/Sled.cs
+++ Assets/Scripts/Sled.cs
@@ -7,56 +7,63 @@ using UnityEngine.Events;
 
 public class Sled : MonoBehaviour
 {
-    public CharacterController sledController;
+   // public CharacterController sledController;
 
+    public Rigidbody rb;
     public float speed = 12f;
     public float gravity = -9.81f;
 
     public float groundDistance = 0.4f;
 
     public Camera sledCamera;
-
+    Vector3 move;
     Vector3 velocity;
     public float jumpVelocity;
 
     public float fallMultiplier = 2.5f;
     public float lowJumpMultiplier = 2f;
  
+    private bool active = false;
      void Start(){
     
-        this.GetComponent<CharacterController>().enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (active){
 
         float x = Input.GetAxis("Horizontal");
       
         float z = Input.GetAxis("Vertical");
 
-        Vector3 move = transform.right * x + transform.forward * z;
+        move = transform.right * x + transform.forward * z;
 
         if (Input.GetKey(KeyCode.LeftShift))
             speed = 24;
         else    
             speed = 12;
 
-        sledController.Move(move * speed * Time.deltaTime);
- 
-        velocity.y += gravity * Time.deltaTime;
-
-        sledController.Move(velocity * Time.deltaTime);
+        }
+       
  
     }
 
  
     public void activateSled(GameObject player){
               
+              active = true;
               player.GetComponentInChildren<Camera>().enabled = false;
               sledCamera.enabled = true;
               sledCamera.GetComponent<MouseLook>().enabled = true;
-              this.GetComponent<CharacterController>().enabled = true;
-              player.GetComponent<CharacterController>().enabled = false;
+
+              //this.GetComponent<CharacterController>().enabled = true;
+             // player.GetComponent<CharacterController>().enabled = false;
+    }
+
+    void FixedUpdate(){
+
+        rb.MovePosition(rb.position + move * 12 * Time.fixedDeltaTime);
+  
     }
 }
